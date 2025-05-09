f2aa7f57354aaa1eb4ab202982c6c1a48f5cc7d5
reecem02 <71314231+reecem02@users.noreply.github.com>
2025-05-08 22:56:22 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import prefetch from "@astrojs/prefetch";
 
 export default defineConfig({
   integrations: [tailwind(), sitemap(), prefetch()],
-  site: "https://adulbrich.github.io",
+  site: "https://reecem02.github.io/demo-github-actions",
   base: "/demo-github-actions",
 });
